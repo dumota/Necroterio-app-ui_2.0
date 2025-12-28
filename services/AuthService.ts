bf3d4a93dda8353713b584e56ffa39801e8a442d
edumota02 <edumota02@gmail.com>
2025-12-28 15:46:26 -0300

    Refactor login function in AuthService to specify return type and improve error handling. Update error return type to match IResponseApi structure for consistency.

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,16 +1,16 @@
 import { ILoginRequest, ILoginResponse } from "@/types/Auth";
 import { postApiV2 } from "./FetchData";
+import { IResponseApi } from "@/types/ResponseApi";
 
-const login = async (account: string, password: string) => {
+const login = async (account: string, password: string):Promise<IResponseApi<ILoginResponse>> => {
   try {
     const response = await postApiV2<ILoginResponse,ILoginRequest>("/login", {
       account,
       password,
     });
     return response;
-    
   } catch (err) {
-    return err;
+    return err as IResponseApi<ILoginResponse>;
   }
 };
 
